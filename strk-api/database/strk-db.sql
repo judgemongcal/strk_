c1ec2038926fc65da904062667f1e269c2a18bd4
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-07-05 17:34:59 +0800

    updated users table to include more user info

--- strk-api/database/strk-db.sql
+++ strk-api/database/strk-db.sql
@@ -1,16 +1,17 @@
 
 CREATE TABLE IF NOT EXISTS users(
 	user_id SERIAL PRIMARY KEY,
-	username VARCHAR(50)
+	username VARCHAR(50),
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
 )
+
 
 CREATE TABLE IF NOT EXISTS habits(
 	habit_id SERIAL PRIMARY KEY,
 	user_id INTEGER REFERENCES users(user_id),
 	habit_name VARCHAR(100) NOT NULL
 )
-
-
 
 CREATE TABLE IF NOT EXISTS habit_entries(
 	entry_id SERIAL PRIMARY KEY,
