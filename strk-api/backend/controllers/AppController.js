4952669072a18e0b5de161c3f3e0b6f3eed8390f
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-07-06 17:16:08 +0800

    fixed add, delete and update endpoints to use params instead of query

--- strk-api/backend/controllers/AppController.js
+++ strk-api/backend/controllers/AppController.js
@@ -25,7 +25,7 @@ exports.addUser = async (req, res) => {
 exports.editUser = async (req, res) => {
 	try {
 		const user = await appModel.updateUser(
-			req.query.id,
+			req.params.id,
 			req.body,
 		);
 		res.status(200).json({
@@ -39,8 +39,9 @@ exports.editUser = async (req, res) => {
 };
 
 exports.deleteUser = async (req, res) => {
+	console.log(req);
 	try {
-		const user = await appModel.deleteUser(req.query.id);
+		const user = await appModel.deleteUser(req.params.id);
 		res.status(200).json({
 			message: `Successfully deleted user.`,
 		});
--- strk-api/backend/routes/AppRoutes.js
+++ strk-api/backend/routes/AppRoutes.js
@@ -4,7 +4,7 @@ const appController = require("../controllers/AppController");
 
 // @TODO: PROVIDE ROUTES
 router.get("/", appController.getUsers);
-router.post("/add-user", appController.addUser);
+router.post("/", appController.addUser);
 router.put("/:id", appController.editUser);
 router.delete("/:id", appController.deleteUser);
 
