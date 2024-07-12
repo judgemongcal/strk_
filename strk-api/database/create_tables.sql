cb43dafb281a712e736114a94b5f0c2ae62f3c49
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-07-12 21:33:54 +0800

    updated users table to include auth credentials

--- strk-api/database/create_tables.sql
+++ strk-api/database/create_tables.sql
@@ -1,7 +1,9 @@
 
 CREATE TABLE IF NOT EXISTS users(
 	user_id SERIAL PRIMARY KEY,
-	username VARCHAR(50),
+	username VARCHAR(50) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	password_hash TEXT NOT NULL,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50)
 )
