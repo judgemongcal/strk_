ae70ccb7b967bd8bb133d0e3ee6a9c1bd279415f
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-07-13 21:14:49 +0800

    created login endpoint and installed dotenv package

--- strk-api/backend/controllers/AuthController.js
+++ strk-api/backend/controllers/AuthController.js
@@ -14,3 +14,36 @@ exports.register = async (req, res) => {
 		res.status(500).json({ error: error.message });
 	}
 };
+
+exports.login = async (req, res) => {
+	const { username, password } = req.body;
+	try {
+		const user = await userModel.getUserByUsername(
+			username,
+		);
+		if (!user) {
+			return res
+				.status(400)
+				.json({ error: "Invalid username or password" });
+		}
+
+		const isMatch = await bcrypt.compare(
+			password,
+			user.password_hash,
+		);
+		if (!isMatch) {
+			return res
+				.status(400)
+				.json({ error: "Invalid username or password" });
+		}
+
+		const token = jwt.sign(
+			{ userId: user.id },
+			process.env.JWT_SECRET,
+			{ expiresIn: "1hr" },
+		);
+		res.status(200).json({ token });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
