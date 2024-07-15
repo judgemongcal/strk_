c41f6ace650d8aec8c232fb800ccc3cc1e78aef5
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-07-15 15:35:43 +0800

    finalized login card ui

--- strk-client/tailwind.config.js
+++ strk-client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-gray": "#242424",
+      },
+    },
   },
   plugins: [],
 };
