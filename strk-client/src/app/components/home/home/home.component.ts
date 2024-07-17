24be656c09bd7681cb43ba3ae5957e19ea14547d
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-07-17 21:30:27 +0800

    fetched current user on homeComponent

--- strk-client/src/app/components/home/home/home.component.ts
+++ strk-client/src/app/components/home/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,26 @@ import { AuthService } from '../../../services/auth.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   router = inject(Router);
   authService = inject(AuthService);
+  userService = inject(UsersService);
+
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    const user_id: any = localStorage.getItem('user_id');
+    this.userService.getUser(user_id).subscribe(
+      (data: any) => {
+        console.log(data);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 
   signOut() {
     try {
--- strk-client/src/app/services/users.service.ts
+++ strk-client/src/app/services/users.service.ts
@@ -7,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class UsersService {
   private apiUrl = `http://localhost:5001/api/users`;
   constructor(private httpClient: HttpClient) {}
+
+  getUser(id: string) {
+    return this.httpClient.get(`${this.apiUrl}/${id}`);
+  }
 }
