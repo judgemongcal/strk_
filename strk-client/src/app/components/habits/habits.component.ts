ae67d268fe54df8c4eb292fbb8259aa5d222a5a3
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-07-19 20:49:08 +0800

    created habits page list ui component

--- strk-client/src/app/components/habits/habits.component.ts
+++ strk-client/src/app/components/habits/habits.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { UsersService } from '../../services/users.service';
+import { HabitsService } from '../../services/habits.service';
 
 @Component({
   selector: 'app-habits',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './habits.component.html',
-  styleUrl: './habits.component.css'
+  styleUrl: './habits.component.css',
 })
-export class HabitsComponent {
+export class HabitsComponent implements OnInit {
+  private router = inject(Router);
+  private authService = inject(AuthService);
+  private userService = inject(UsersService);
+  private habitsService = inject(HabitsService);
+  private user_id: any = localStorage.getItem('user_id');
 
+  lookups: any = {
+    user_info: [],
+    habits: [],
+  };
+
+  ngOnInit(): void {
+    this.getCurrentUser();
+    this.getUserHabits();
+  }
+
+  getCurrentUser() {
+    this.userService.getUser(this.user_id).subscribe(
+      (data: any) => {
+        this.lookups.user_info = data[0];
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+  getUserHabits() {
+    this.habitsService.getHabits(this.user_id).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.lookups.habits = data;
+        console.log(this.lookups);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
