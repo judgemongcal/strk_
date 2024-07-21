d59b220dc6f7ad90e110d8ad0c4fbb0d577d3d72
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-07-21 17:38:34 +0800

    rendered all units in habits-add

--- strk-client/src/app/components/habit-add/habit-add.component.ts
+++ strk-client/src/app/components/habit-add/habit-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HabitsService } from '../../services/habits.service';
 import {
@@ -8,11 +9,12 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { UnitsService } from '../../services/units.service';
 
 @Component({
   selector: 'app-habit-add',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, NgFor],
   templateUrl: './habit-add.component.html',
   styleUrl: './habit-add.component.css',
 })
@@ -20,15 +22,25 @@ export class HabitAddComponent implements OnInit {
   private router = inject(Router);
   private activatedRoute = inject(ActivatedRoute);
   private habitsService = inject(HabitsService);
+  private unitsService = inject(UnitsService);
   private user_id: any = null;
   private fb = inject(FormBuilder);
   form: FormGroup = this.fb.group({
     habit_name: new FormControl('', [Validators.required]),
   });
 
+  lookups: any = {
+    units: [],
+  };
+
   ngOnInit(): void {
     this.user_id = this.activatedRoute.params.subscribe((params) => {
       this.user_id = params['id'];
+    });
+
+    this.unitsService.getUnits().subscribe((data) => {
+      this.lookups.units = data;
+      console.log(this.lookups.units);
     });
   }
 
