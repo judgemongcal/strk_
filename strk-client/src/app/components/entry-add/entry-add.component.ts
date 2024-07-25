b27ace204c6384c176d56a66bcecd327ce502cbb
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-07-25 21:37:18 +0800

    created entry-add component and added it into the routes

---
+++ strk-client/src/app/components/entry-add/entry-add.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-add',
+  standalone: true,
+  imports: [],
+  templateUrl: './entry-add.component.html',
+  styleUrl: './entry-add.component.css'
+})
+export class EntryAddComponent {
+
+}
