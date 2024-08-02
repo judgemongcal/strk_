86fc92e4faddaaa37a5233692b2993a4a61c6fa6
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-08-02 14:32:07 +0800

    added redirection to login page on init of app.ts

--- strk-client/src/app/app.component.ts
+++ strk-client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import CalHeatmap from 'cal-heatmap';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,14 @@ import CalHeatmap from 'cal-heatmap';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'strk-client';
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    if (!this.authService.isAuthenticated()) {
+      this.router.navigate(['']);
+    }
+  }
 }
