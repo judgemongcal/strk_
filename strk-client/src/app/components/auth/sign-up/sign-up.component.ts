66e5ffd0a699ccd3adeb11268fb9d5f75bc34d06
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-08-04 17:17:41 +0800

    created base layout of sign-up page

--- strk-client/src/app/components/auth/sign-up/sign-up.component.ts
+++ strk-client/src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersService } from '../../../services/users.service';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  fb = inject(FormBuilder);
+  router = inject(Router);
+  authService = inject(AuthService);
+  userService = inject(UsersService);
 
+  form: FormGroup = this.fb.group({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  ngOnInit(): void {}
+
+  signUp() {}
+
+  handleSignInRedirect() {
+    this.router.navigate(['']);
+  }
 }
