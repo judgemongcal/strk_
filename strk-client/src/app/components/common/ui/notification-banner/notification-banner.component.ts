2665460afb15e91b61c1c004de1fca7c9eac0690
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-08-05 15:18:53 +0800

    created notification interface and created base functions of notification-banner component

--- strk-client/src/app/components/common/ui/notification-banner/notification-banner.component.ts
+++ strk-client/src/app/components/common/ui/notification-banner/notification-banner.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { NotificationService } from '../../../../services/notification.service';
 
 @Component({
   selector: 'app-notification-banner',
   standalone: true,
   imports: [],
   templateUrl: './notification-banner.component.html',
-  styleUrl: './notification-banner.component.css'
+  styleUrl: './notification-banner.component.css',
 })
-export class NotificationBannerComponent {
+export class NotificationBannerComponent implements OnInit {
+  private notificationService = inject(NotificationService);
+  message: string = '';
+  isShown: boolean = false;
 
+  ngOnInit(): void {
+    this.notificationService.notification$.subscribe((notification: any) => {
+      this.message = notification.message;
+      this.isShown = notification.isShown;
+    });
+  }
+
+  dismissNotification(): void {
+    this.notificationService.dismiss();
+  }
 }
