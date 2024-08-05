d86f21491bf529aa9f1980cf43956f92aa077351
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-08-05 15:14:33 +0800

    created notification-banner component and notification service

---
+++ strk-client/src/app/components/common/ui/notification-banner/notification-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './notification-banner.component.html',
+  styleUrl: './notification-banner.component.css'
+})
+export class NotificationBannerComponent {
+
+}
