ecf34b30c3922743398223d38b23c5f8b734c4fa
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-08-05 15:22:33 +0800

    wrote notificationService function

--- strk-client/src/app/services/notification.service.ts
+++ strk-client/src/app/services/notification.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Notification } from '../interfaces/notification';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationService {
+  private notificationObject = new BehaviorSubject<Notification>({
+    message: '',
+    isShown: false,
+  });
+  notification$ = this.notificationObject.asObservable();
 
-  constructor() { }
+  show(message: string): void {
+    this.notificationObject.next({ message, isShown: true });
+  }
+
+  dismiss(): void {
+    this.notificationObject.next({ message: '', isShown: false });
+  }
 }
